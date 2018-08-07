dc69cb5ed1f21b34c0c0615c41366f5261bc0bcb
Kevin Brown <kevinbrown2354@gmail.com>
2018-08-07 14:17:11 +0300

    (feat) add admin control panel
    Add scaffolding for admin control panel

---
+++ components/com_cajobboard/admin/Controller/JobPosting.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Administrator Job Posting Controller
+ *
+ * @package   Calligraphic Job Board
+ * @version   0.1 May 1, 2018
+ * @author    Calligraphic, LLC http://www.calligraphic.design
+ * @copyright Copyright (C) 2018 Calligraphic, LLC
+ * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
+ *
+ */
+
+namespace Calligraphic\Cajobboard\Admin\Controller;
+
+// Framework classes
+use FOF30\Container\Container;
+use FOF30\Controller\DataController;
+use FOF30\View\Exception\AccessForbidden;
+
+// Component classes
+use Calligraphic\Cajobboard\Admin\Controller\Mixin;
+
+// no direct access
+defined('_JEXEC') or die;
+
+class JobPostings extends DataController
+{
+  use Mixin\PredefinedTaskList;
+
+	/**
+	 * Overridden. Limit the tasks we're allowed to execute.
+	 *
+	 * @param   Container $container
+	 * @param   array     $config
+	 */
+	public function __construct(Container $container, array $config = array())
+	{
+    parent::__construct($container, $config);
+  }
+
+	/**
+	 * Runs before the browse task.
+	 */
+	protected function onBeforeBrowse()
+	{
+  }
+
+	/**
+	 * Runs before the read task.
+	 */
+	protected function onBeforeRead()
+	{
+  }
+
+	/**
+	 * Performs auth checks before saving subscription data
+	 *
+	 * @return bool
+	 */
+	protected function onBeforeSave()
+	{
+  }
+}
