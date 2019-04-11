edc012b160b8b86bd6492453d4b804546d7b6ddb
Kevin Brown <kevinbrown2354@gmail.com>
2019-04-11 15:41:48 +0300

    feat(copyright module) add copyright module
    Add copyright module, forked from AJO Copyright

---
+++ modules/mod_ca_copyright/tmpl/default.php
@@ -0,0 +1,31 @@
+<?php
+  /**
+   * Calligraphic Job Board Copyright Module
+   *
+   * This partial is used to display the component portion of a page for printing
+   *
+   * @package     Calligraphic Job Board
+   *
+   * @version     0.1 May 1, 2018
+   * @author      Calligraphic, LLC http://www.calligraphic.design
+   * @copyright   Copyright (C) 2018 Calligraphic, LLC, (C) 2016 freakout.be All rights reserved.
+   * @license     http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
+   *
+   */
+
+   // no direct access
+   defined('_JEXEC') or die;
+?>
+
+<div class="<?php echo $moduleclass_sfx ?>">
+  <?php echo $copyright; ?><?php echo $year; ?>
+
+  <?php if ($showSitename==1):?>
+    &nbsp;<?php echo $csiteName; ?>
+  <?php endif;?>
+
+  <?php if ($params -> get('freeText')):?>
+    &nbsp;<?php echo $params -> get('freeText'); ?>
+  <?php endif;?>
+</div>
+
